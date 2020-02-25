1496f1fd485186b333f816420a63a29f28b1f175
Dariusz Okonski <darek200180@gmail.com>
2020-02-25 13:05:26 +0100

    Updates

---
+++ precache-manifest.75c5ae122989fe01ad5c788d85bd7611.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c0ebe177552a7e240559ad99a259d93",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "13bfb28b1695b57f695a",
+    "url": "/expense-tracker/static/css/main.62ba5791.chunk.css"
+  },
+  {
+    "revision": "7dd68a108f36bc335a6c",
+    "url": "/expense-tracker/static/js/2.2002e6db.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/expense-tracker/static/js/2.2002e6db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13bfb28b1695b57f695a",
+    "url": "/expense-tracker/static/js/main.ae8316ac.chunk.js"
+  },
+  {
+    "revision": "72584990847f84a51217",
+    "url": "/expense-tracker/static/js/runtime-main.d05b2cef.js"
+  }
+]);
